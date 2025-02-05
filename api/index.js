f43cf05c0b7fe4b5c248a561bc10814b492eaeee
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2025-02-05 20:13:25 +1100

    "Database connection and .env initialisation"

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGOURI).then(
+    () => { console.log("Database connected") }
+).catch((err) => {
+    console.log(err)
+})
 
 const app = express();
 
