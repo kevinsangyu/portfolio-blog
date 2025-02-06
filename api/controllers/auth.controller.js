a63f536439c102f7a101218ccebc7daf712c1a9e
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2025-02-06 18:35:41 +1100

    functionality to signup page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
+    console.log("Signup request receiver with body: ", req.body)
 
     if (!username || !email || !password || username === "" || email === "" || password === "") {
         next(errorHandler(400, 'All fields are required'))
