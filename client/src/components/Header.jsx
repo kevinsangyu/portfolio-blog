c9aef5ab040573a7ac7d5ee44066506d9f6b7925
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2025-02-12 22:24:53 +1100

    dark mode complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { Button, Navbar, NavbarToggle, TextInput, Avatar, Dropdown, DropdownHeader, DropdownItem } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
-import { FaMoon } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
+import { FaMoon, FaSun } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTheme } from '../redux/theme/themeSlice'
 
 export default function Header() {
     const path = useLocation().pathname
     const { currentUser } = useSelector(state => state.user)
+    const { theme } = useSelector((state) => state.theme)
+    const dispatch = useDispatch()
   return (
     <Navbar className='border-b-2'>
             {/* Below is a link to the portfolio when it deploys. */}
@@ -25,8 +28,8 @@ export default function Header() {
             <AiOutlineSearch />
         </Button>
         <div className='flex gap-2 md:order-2'>
-            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-                <FaMoon />
+            <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={() =>dispatch(toggleTheme())}>
+                {theme === 'light' ? <FaSun /> : <FaMoon />}
             </Button>
             {currentUser ? (
                 <Dropdown className='' arrowIcon={false} inline label={<Avatar alt='usericon' img={currentUser.profilePicture} rounded/>}>
