09bfcb445302e174b16ff50c27abf4c77246cb66
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2025-02-20 17:53:12 +1100

    edit/update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -55,11 +55,30 @@ export const getposts = async (req, res, next) => {
 
 export const deletepost = async (req, res, next) => {
     if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-        return next(errorHandler(403, 'You are not allowed to deleted this post'))
+        return next(errorHandler(403, 'You are not allowed to delete this post'))
     }
     try {
         await Post.findByIdAndDelete(req.params.postId)
         res.status(200).json('The post has been deleted')
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const updatepost = async (req, res, next) => {
+    if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+        return next(errorHandler(403, 'You are not allowed to edit this post'))
+    }
+    try {
+        const updatedPost = await Post.findByIdAndUpdate(req.params.postId, {
+            $set: {
+                title: req.body.title,
+                content: req.body.content,
+                category: req.body.category,
+                image: req.body.image,
+            }
+        }, {new: true})
+        res.status(200).json(updatedPost)
     } catch (error) {
         next(error)
     }
