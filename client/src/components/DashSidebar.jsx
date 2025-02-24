f0afaa15c318b69bd791728734707ba5455ea67a
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2025-02-25 10:59:04 +1100

    dashboard overview component to admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {Sidebar} from 'flowbite-react'
-import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser, HiAnnotation} from 'react-icons/hi'
+import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser, HiAnnotation, HiChartPie} from 'react-icons/hi'
 import { useDispatch } from 'react-redux'
 import { signOutSuccess } from '../redux/user/userSlice'
 import { useSelector } from 'react-redux'
@@ -37,6 +37,13 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
             <Sidebar.ItemGroup className='flex flex-col gap-1'>
+                {currentUser.isAdmin && (<>
+                    <Link to='/dashboard?tab=dashboard'>
+                        <Sidebar.Item active={tab === 'dashboard' || !tab} icon={HiChartPie} as='div'>
+                            Dashboard
+                        </Sidebar.Item>
+                    </Link>
+                </>)}
                 <Link to='/dashboard?tab=profile'>
                     <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={currentUser.isAdmin ? "Admin" : "User"} labelColor='dark' as='div'>
                         Profile
