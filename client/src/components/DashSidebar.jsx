4098787348133bf465a076360150eec60ac5d11c
Kevin <74086991+kevinsangyu@users.noreply.github.com>
2025-02-25 10:20:58 +1100

    comment section in admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import {Sidebar} from 'flowbite-react'
-import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
+import {HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser, HiAnnotation} from 'react-icons/hi'
 import { useDispatch } from 'react-redux'
 import { signOutSuccess } from '../redux/user/userSlice'
 import { useSelector } from 'react-redux'
@@ -51,6 +51,11 @@ export default function DashSidebar() {
                     <Link to='/dashboard?tab=users'>
                         <Sidebar.Item active={tab === 'users'} icon={HiOutlineUserGroup} as='div'>
                             Users
+                        </Sidebar.Item>
+                    </Link>
+                    <Link to='/dashboard?tab=comments'>
+                        <Sidebar.Item active={tab === 'comments'} icon={HiAnnotation} as='div'>
+                            Comments
                         </Sidebar.Item>
                     </Link>
                 </>)}
